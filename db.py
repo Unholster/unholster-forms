25fb537d645fdd7f88158f961412558929ca6917
Sebastian Acuna <sacuna@gmail.com>
2013-05-24 20:32:40 -0400

    Fixed bug with database url

--- db.py
+++ db.py
@@ -3,5 +3,6 @@ from settings import MONGODB_URI
 from urlparse import urlparse
 
 db_name = getattr(urlparse(MONGODB_URI), 'path') or  'unholster'
+db_name = db_name.lstrip("/")
 client = MongoClient(MONGODB_URI)
 db = client[db_name]
