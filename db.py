a20cb408d291315124ffaa664e872b2a02643214
Sebastian Acuna <sacuna@gmail.com>
2013-05-24 20:27:50 -0400

    Handling of database in mongodb uri

--- db.py
+++ db.py
@@ -1,5 +1,7 @@
 from pymongo import MongoClient
 from settings import MONGODB_URI
+from urlparse import urlparse
 
+db_name = getattr(urlparse(MONGODB_URI), 'path') or  'unholster'
 client = MongoClient(MONGODB_URI)
-db = client['unholster']
+db = client[db_name]
